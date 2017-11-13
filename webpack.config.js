e0f80437be3bbf1e68d9ef292de4f6200cbb66f4
Isaiah Odhner <isaiahodhner@gmail.com>
2017-11-12 21:38:59 -0500

    Move app and setup deploying to itch.io

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,13 @@ module.exports = {
 	entry: './src/app.js',
 	output: {
 		filename: 'app-bundle.js',
-		path: path.resolve(__dirname, 'build'),
+		path: path.resolve(__dirname, 'app/build'),
 		publicPath: '/build/',
 	},
 	devServer: {
+		contentBase: './app',
 		public: process.env.C9_HOSTNAME,
-		host: process.env.IP || "127.0.0.1",
-		port: process.env.PORT || "8080",
+		host: process.env.IP || '127.0.0.1',
+		port: process.env.PORT || '8080',
 	}
 };
